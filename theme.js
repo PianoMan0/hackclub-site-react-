50ba27db6c42071db24126bff74280ea73916010
PianoMan0 <180116946+PianoMan0@users.noreply.github.com>
2025-05-20 15:20:19 -0400

    Create theme.js

---
+++ theme.js
@@ -0,0 +1,27 @@
+export default {
+  colors: {
+    text: "#17171d",
+    background: "#fff",
+    primary: "#ec3750",
+    muted: "#f6f6f6"
+  },
+  fonts: {
+    body: "-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, sans-serif"
+  },
+  buttons: {
+    primary: {
+      bg: "primary",
+      color: "background",
+      borderRadius: 999,
+      fontWeight: "bold"
+    },
+    outline: {
+      bg: "transparent",
+      color: "background",
+      border: "2px solid",
+      borderColor: "background",
+      borderRadius: 999,
+      fontWeight: "bold"
+    }
+  }
+};
