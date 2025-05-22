79f63ee85adcd2ed0884664ce65411528ddfe366
PianoMan0 <180116946+PianoMan0@users.noreply.github.com>
2025-05-22 14:14:27 -0400

    Update theme.js

--- theme.js
+++ theme.js
@@ -1,9 +1,19 @@
 export default {
+  initialColorModeName: "light",
+  useColorSchemeMediaQuery: true,
   colors: {
     text: "#17171d",
     background: "#fff",
     primary: "#ec3750",
-    muted: "#f6f6f6"
+    muted: "#f6f6f6",
+    modes: {
+      dark: {
+        text: "#f6f6f6",
+        background: "#17171d",
+        primary: "#ff4852",
+        muted: "#23232b"
+      }
+    }
   },
   fonts: {
     body: "-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, sans-serif"
