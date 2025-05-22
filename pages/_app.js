af3acaecbe82e18de03a96078d1218c9fbad0ccf
PianoMan0 <180116946+PianoMan0@users.noreply.github.com>
2025-05-22 16:12:18 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,22 @@
+/** @jsxImportSource theme-ui */
 import { ThemeProvider } from "theme-ui";
+import { useState, useEffect } from "react";
 import theme from "../theme";
 
 export default function App({ Component, pageProps }) {
+  // Local storage functionality cuz I'm just that cool
+  const [colorMode, setColorMode] = useState("light");
+  useEffect(() => {
+    const saved = typeof window !== "undefined" && window.localStorage.getItem("color-mode");
+    if (saved) setColorMode(saved);
+  }, []);
+  useEffect(() => {
+    if (typeof window !== "undefined") window.localStorage.setItem("color-mode", colorMode);
+  }, [colorMode]);
+
   return (
-    <ThemeProvider theme={theme}>
-      <Component {...pageProps} />
+    <ThemeProvider theme={{ ...theme, initialColorModeName: "light" }} colorMode={colorMode} setColorMode={setColorMode}>
+      <Component {...pageProps} colorMode={colorMode} setColorMode={setColorMode} />
     </ThemeProvider>
   );
 }
